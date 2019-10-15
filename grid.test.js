4a373034fe79370bd005e0e470d97d7aa19f3546
David <mail@davidfloegel.com>
2019-10-15 11:00:32 +0100

    get started on logic that checks whether user can connect 2 dots

--- grid.test.js
+++ grid.test.js
@@ -1,3 +1,13 @@
+/**
+ * Generates top left coordinates for the boxes that are being generated
+ *
+ *  1---2---3
+ *  |   |   |
+ *  4---5---6
+ *  |   |   |
+ *  7---8---9
+ *
+ */
 const makeGrid = dots => {
   let grid = {};
 
@@ -23,6 +33,37 @@ const makeGrid = dots => {
   });
 
   return grid;
+};
+
+const areDotsConnectable = (grid, pointA, pointB) => {
+  let connectable = false;
+
+  Object.keys(grid).forEach(box => {
+    const coords = grid[box];
+    const [topLeft, topRight, bottomLeft, bottomRight] = coords;
+
+    // No this is shit
+  });
+
+  return "what";
+};
+
+const connectDots = (grid, connections = {}, user, pointA, pointB) => {
+  // TODO check if the two dots are connectable
+
+  const lower = pointA < pointB ? pointA : pointB;
+  const higher = pointA > pointB ? pointA : pointB;
+  const key = `${lower}-${higher}`;
+
+  const connectable = areDotsConnectable(grid, lower, higher);
+
+  if (connections[key]) {
+    return connections;
+  }
+
+  connections[key] = user;
+
+  return connections;
 };
 
 describe("Grid", () => {
@@ -57,5 +98,76 @@ describe("Grid", () => {
       8: [10, 11, 14, 15],
       9: [11, 12, 15, 16]
     });
+  });
+});
+
+describe("Are dots connectable?", () => {
+  it("returns true if dots are connectable", () => {
+    const grid = makeGrid(3);
+
+    expect(areDotsConnectable(grid, 1, 2)).toBe(true);
+    expect(areDotsConnectable(grid, 1, 4)).toBe(true);
+
+    expect(areDotsConnectable(grid, 2, 1)).toBe(true);
+    expect(areDotsConnectable(grid, 2, 3)).toBe(true);
+    expect(areDotsConnectable(grid, 2, 5)).toBe(true);
+
+    expect(areDotsConnectable(grid, 3, 2)).toBe(true);
+    expect(areDotsConnectable(grid, 3, 6)).toBe(true);
+
+    expect(areDotsConnectable(grid, 4, 1)).toBe(true);
+    expect(areDotsConnectable(grid, 4, 5)).toBe(true);
+    expect(areDotsConnectable(grid, 4, 7)).toBe(true);
+
+    expect(areDotsConnectable(grid, 5, 2)).toBe(true);
+    expect(areDotsConnectable(grid, 5, 4)).toBe(true);
+    expect(areDotsConnectable(grid, 5, 6)).toBe(true);
+    expect(areDotsConnectable(grid, 5, 8)).toBe(true);
+  });
+
+  it("returns false if dots are not connectable", () => {
+    const grid = makeGrid(3);
+
+    expect(areDotsConnectable(grid, 1, 5)).toBe(false);
+    expect(areDotsConnectable(grid, 3, 5)).toBe(false);
+    expect(areDotsConnectable(grid, 5, 9)).toBe(false);
+    expect(areDotsConnectable(grid, 6, 7)).toBe(false);
+    expect(areDotsConnectable(grid, 6, 8)).toBe(false);
+  });
+});
+
+describe("User Interaction", () => {
+  it("connects two dots", () => {
+    const grid = makeGrid(3);
+    const user = 1;
+    const pointA = 5;
+    const pointB = 3;
+    const connected = connectDots(grid, {}, user, pointA, pointB);
+
+    expect(connected).toEqual({
+      "3-5": user
+    });
+  });
+
+  it("does nothing if the two dots are already selected", () => {
+    const grid = makeGrid(3);
+    const user = 1;
+    const pointA = 3;
+    const pointB = 5;
+    const connected = connectDots(grid, { "3-5": 2 }, user, pointA, pointB);
+
+    expect(connected).toEqual({
+      "3-5": 2
+    });
+  });
+
+  it("does nothing if the user can not connect two dots", () => {
+    const grid = makeGrid(3);
+    const user = 1;
+    const pointA = 1;
+    const pointB = 7;
+    const connected = connectDots(grid, {}, user, pointA, pointB);
+
+    expect(connected).toEqual({});
   });
 });
